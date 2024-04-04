c3ee1b2706b0b79c061bfc1f56eb768f1c4f14d9
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-04 21:12:13 +0300

    fix lfu

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -34,7 +34,8 @@ class LFUCache(BaseCaching):
             else:
                 # get the least frequently used item in an Ordered Dict
                 # sort the dict by values and get the first tuple
-                least_use = sorted(list(self.use_order.items())[0:4],
+                print(self.use_order)
+                least_use = sorted(list(self.use_order.items())[0:BaseCaching.MAX_ITEMS],
                                    key=lambda x: x[1])[0]
                 del self.cache_data[least_use[0]]
                 del self.use_order[least_use[0]]
