8e5bd39d34f56901ead7d4b4b8f2be90624d8574
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-09 20:32:05 +0300

    fix style issues

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,19 +1,31 @@
+#!/usr/bin/env python3
+"""Define locale selector for babel"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config():
+    """
+    Configuration for some flask variables
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
 app.config.from_object(Config())
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request headers"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def home():
+    """Home route"""
     return render_template('1-index.html')
